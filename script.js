0abecc215da62a65912d21551d54cc19e2efcee1
Misha Alia Awad <awadmishaa@gmail.com>
2023-11-10 18:49:55 +0900

    add individual button value to button class

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let displayValues;
 const calcScreen = document.querySelector('.calc-screen');
 const buttonField = document.querySelector('.buttons');
 const numberButtons = document.querySelector('.num-buts');
-let operatorButtons = document.querySelector('.operator-buts');
+const operatorButtons = document.querySelector('.operator-buts');
 const numButtons = new DocumentFragment;
 const oButtons = new DocumentFragment;
 let numList = [];
@@ -27,7 +27,7 @@ for (let i = 0; i < 10; i++){
 numList.forEach(n => {
     const nButton = document.createElement('span');
     nButton.textContent = n;
-    nButton.className = 'numButton';
+    nButton.className = `numButton ${n}`;
     numButtons.appendChild(nButton);
 });
 numberButtons.append(numButtons)
@@ -39,7 +39,7 @@ buttonField.append(numButtons);
 operators.forEach(o => {
     const oButton = document.createElement('span');
     oButton.textContent = o;
-    oButton.className = 'operatorButton';
+    oButton.className = `operatorButton ${o}`;
     oButtons.appendChild(oButton);
 });
 
@@ -83,18 +83,42 @@ const operate = function(operator, n1, n2){
     };
 };
 
-// function that opulate the display when you click the number buttons
+// function that populates the display when you click the number buttons
 
 numberButtons.addEventListener('click', e => {
     calcScreen.innerText = e.target.innerText;
 });
 
+//function that stores the values of the buttons pressed in variable1 and variable2
 
+numberButtons.addEventListener('click', e => {
+    if(variable1 === undefined && variable2 === undefined){
+        variable1 = e.target.innerText;
+    } else if (variable1 !== undefined && variable2 === undefined) {
+        variable2 = e.target.innerText;
+    } else if (variable1 !== undefined && variable2 !== undefined){
+        variable1 = e.target.innerText;
+    }
+});
 
-// function that takes variable and performs calculation
+// function that stores the value of the operator button pressed
 
-operatorButtons.addEventListener('click', (e)=> {
-    
+operatorButtons.addEventListener('click', e => {
+    operator = e.target.innerText;
 })
+
+// function that calculates on the press of an operator
+
+operatorButtons.addEventListener('click', e => {
+    let answer;
+    if (variable1 === undefined && variable2 === undefined) {
+        return;
+    } else if (variable1 !== undefined) {
+        answer = operate(operator, Number(variable1), Number(variable2));
+        console.log(answer)
+    }
+})
+
+
 
 
