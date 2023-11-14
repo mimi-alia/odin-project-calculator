a90cdc8b0cac6e3617b6faa026d745f8479b8d6e
Misha Alia Awad <awadmishaa@gmail.com>
2023-11-15 03:51:31 +0900

    create list of all spans with button class to iterate over

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const oButtons = new DocumentFragment;
 let numList = [];
 const operators = ['/', 'x', '-', '+', '='];
 
+
+
 // create list of numbers to populate number buttons
 
 for (let i = 0; i < 10; i++){
@@ -27,10 +29,11 @@ for (let i = 0; i < 10; i++){
 numList.forEach(n => {
     const nButton = document.createElement('span');
     nButton.textContent = n;
-    nButton.className = `numButton ${n}`;
+    nButton.className = `button numButton ${n}`;
     numButtons.appendChild(nButton);
 });
 numberButtons.append(numButtons)
+
 
 // add document fragment to the button field
 
@@ -38,13 +41,13 @@ numberButtons.append(numButtons)
 operators.forEach(o => {
     const oButton = document.createElement('span');
     oButton.textContent = o;
-    oButton.className = `operatorButton ${o}`;
+    oButton.className = `button operatorButton ${o}`;
     oButtons.appendChild(oButton);
 });
 
 operatorButtons.append(oButtons)
 
-
+let allButtons = document.querySelectorAll('.button');
 
 /*Calculator functions */
 
@@ -97,18 +100,43 @@ updateDisplay();
 //     console.log('hi');
 // }
 
+buttonField.addEventListener('click', e => {
+    console.log(e.target.classList[0])
+    console.log(displayValue)
+    if (e.target.classList[0] === 'numButton'){
+        if(displayValue == '0') {
+            displayValue=e.target.classList[1];
+        } else if (displayValue !== '0'){
+            displayValue+=e.target.classList[1];
+        }
+        updateDisplay();
+        console.log('You selected the number value: ' + e.target.classList[1]);
+    } else if (e.target.classList[0] === 'operatorButton' && e.target.classList[1] !== '=') {
+        if (variable1 === undefined){
+            variable1 = Number(displayValue)
+
+        } else if (variable1 !== undefined && variable2 === undefined){
+
+        }
+    } else if (e.target.classList[0] === 'operatorButton' && e.target.classList[1] === '=') {}
+})
+
+//how do you update the display so that on click of the next number after a non-equals-sign is clicked, it regenerates
+
+
 
 //function that stores the values of the buttons pressed in variable1 and variable2
 
-numberButtons.addEventListener('click', e => {
-    if(variable1 === undefined && variable2 === undefined){
-        variable1 = e.target.innerText;
-    } else if (variable1 !== undefined && variable2 === undefined) {
-        variable2 = e.target.innerText;
-    } else if (variable1 !== undefined && variable2 !== undefined){
-        variable1 = e.target.innerText;
-    }
-});
+// numberButtons.addEventListener('click', e => {
+//     if(variable1 === undefined && variable2 === undefined){
+//         variable1 = e.target.innerText;
+//         displayValue += variable1
+//     } else if (variable1 !== undefined && variable2 === undefined) {
+//         variable2 = e.target.innerText;
+//     } else if (variable1 !== undefined && variable2 !== undefined){
+//         variable1 = e.target.innerText;
+//     }
+// });
 
 // function that stores the value of the operator button pressed
 
@@ -127,7 +155,3 @@ operatorButtons.addEventListener('click', e => {
         console.log(answer)
     }
 })
-
-
-
-
